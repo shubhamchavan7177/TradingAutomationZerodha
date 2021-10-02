80773ab981365377015e1b055a8193c4756ba1af
shubham_chavan <shubhamchavan7177@gmail.com>
2021-10-02 21:46:29 +0530

    initial setup

---
+++ src/main/java/com/example/accessingdatamysql/UserRepository.java
@@ -0,0 +1,13 @@
+package com.example.accessingdatamysql;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+    List<User> findByEmail(String email);
+    List<User> findByName(String name);
+}
